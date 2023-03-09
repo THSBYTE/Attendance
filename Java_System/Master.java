97dda6444a1107d69482468b7c2be4e93cdb46ff
Olivia Dean <oliviamdean06@gmail.com>
2023-03-08 20:34:16 -0600

    Updating the Master class

--- Java_System/Master.java
+++ Java_System/Master.java
@@ -1,6 +1,23 @@
-package BYTE;
+import java.util.*;
 public class Master{
-    public static void main(String[] args){
-        //do things...
+    Map<String,Student> studentList = new HashMap<String,Student>();
+    List<ScanEvent> eventList = new LinkedList<ScanEvent>();
+    public void addAttendance(Student student){
+        studentList.put(student.getID(),student);
+    }
+
+    public void scan(ScanEvent scan){
+        eventList.add(scan);
+        studentList.get(scan.getID()).addEvent(scan);
+    }
+
+    public void printEventList(){
+        for(ScanEvent se: eventList)
+            System.out.println("["+se.getID()+"]    :   "+studentList.get(se.getID()).getName()+" :    "+studentList.get(se.getID()).getGrade()+"    :    "+se.getTime()+"  :");
+    }
+
+    public void printStudentList(){
+        for (Map.Entry<String,Student> entry : studentList.entrySet()) 
+            System.out.println("ID:" + entry.getKey() +",   NAME:" + entry.getValue());
     }
 }
